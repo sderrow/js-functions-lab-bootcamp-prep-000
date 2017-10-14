141dcffcbca14b78ffa18a7abfd940a7dfd73f42
Sean Derrow <sderrow@mit.edu>
2017-10-14 19:25:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday, ${name}}!"
+  return `Happy ${holiday, ${name}}!`
 }
 
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
