f9775aaaa7507e97b040af00cd385529e4cf38fe
Sean Derrow <sderrow@mit.edu>
2017-10-14 19:24:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
+
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday, ${name}}!"
+}
+
